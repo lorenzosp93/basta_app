88a73f21d464457a1dc6d66a61d34793957dbfbc
lorenzosp93 <lorenzosp93@gmail.com>
2020-04-20 10:16:13 +0200

    deployment checklist

--- basta_app/settings.py
+++ basta_app/settings.py
@@ -20,13 +20,19 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '_zj@s*0occg1$klsgd$48v42w$zv1x=p48qt@0ftea8g_5x7np'
+#SECRET_KEY = '_zj@s*0occg1$klsgd$48v42w$zv1x=p48qt@0ftea8g_5x7np'
+SECRET_KEY = os.getenv('DJANGO_SECRET_KEY', '_zj@s*0occg1$klsgd$48v42w$zv1x=p48qt@0ftea8g_5x7np')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+# DEBUG = True
+DEBUG = os.getenv('DJANGO_DEBUG', 'True') != 'False'
 
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = [
+    'basta-app-stage.herokuapp.com/',
+    'basta-app.herokuapp.com/',
+]
+if DEBUG:
+    ALLOWED_HOSTS.append('*')
 
 # Application definition
 
@@ -42,6 +48,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -81,6 +88,10 @@ DATABASES = {
     }
 }
 
+# Heroku: Update database configuration from $DATABASE_URL.
+import dj_database_url
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
 
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
@@ -119,6 +130,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+# Simplified static file serving.
+# https://warehouse.python.org/project/whitenoise/
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Redirect to home URL after login (Default redirects to /accounts/profile/)
 LOGIN_REDIRECT_URL = '/'
