cddb2f6714a1e5984d9d666983d01915599c4af5
lorenzosp93 <lorenzosp93@gmail.com>
2020-04-20 23:15:41 +0200

    fix round template

--- basta/views.py
+++ basta/views.py
@@ -21,7 +21,6 @@ class PlayView(AjaxableResponseMixin, UpdateView):
         round_ = Round.objects.get(session=session, number=self.kwargs.get('number'))
         return round_.play_set.get(user=self.request.user)
     
-    @login_required
     def get(self, request, *args, **kwargs):
         return super().get(request, *args, **kwargs)
 
@@ -98,6 +97,7 @@ class SessionListView(ListView):
     model = Session
     context_object_name = "sessions"
 
+@login_required
 def play_create(request, slug, number):
     session = Session.objects.get(slug=slug)
     round_ = Round.objects.get(number=number, session=session)
@@ -117,6 +117,7 @@ def play_create(request, slug, number):
                 context={'error': _('User is not authenticated')}
             )
 
+@login_required
 def play_score(request, slug, number):
     session = Session.objects.get(slug=slug)
     round_ = Round.objects.get(number=number, session=session)
@@ -133,6 +134,7 @@ def play_score(request, slug, number):
         reverse('basta:round', kwargs={'slug':slug, 'number':number})
     )
 
+@login_required
 def round_create(request, slug):
     session = Session.objects.get(slug=slug)
     if not session.round_set.filter(active=True):
@@ -146,13 +148,14 @@ def round_create(request, slug):
     else:
         return redirect(reverse('basta:session', kwargs={'slug':slug}))
 
+@login_required
 def session_create(request):
     name = request.POST.get('session_name', '')
     new_session = Session.objects.create(
         name=name,
     )
     return redirect(reverse("basta:session", kwargs={'slug':new_session.slug}))
-
+@login_required
 def session_close(request, slug):
     session = Session.objects.get(slug=slug)
     session.active = False
