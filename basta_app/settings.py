0ddb7260f134fc9da0f2a285ec701412b8226cf0
lorenzosp93 <lorenzosp93@gmail.com>
2020-05-17 20:09:25 +0200

    email fixes

--- basta_app/settings.py
+++ basta_app/settings.py
@@ -113,7 +113,6 @@ EMAIL_HOST_PASSWORD = os.getenv('SENDGRID_PASSWORD', '')
 EMAIL_HOST= 'smtp.sendgrid.net'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-DEFAULT_FROM_EMAIL = 'me@lorenzosp.com'
 
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
